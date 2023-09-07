91d7939af47b70c9a16c02e486703bd88d59f5f0
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2023-09-07 23:17:11 +0200

    Add bonus exercise on summing in alternative ways

--- src/exercise1.rs
+++ src/exercise1.rs
@@ -8,6 +8,16 @@ fn sum_lines(lines: Vec<String>) -> u8 {
     convert_to_integers(lines).into_iter().sum()
 }
 
+/// Accepts a vector of the lines in the exercise and returns their sum
+/// # Bonus!
+/// Try implementing this as with [sum_lines], but using a different method than that one!
+fn sum_lines_bonus(lines: Vec<String>) -> u8 {
+    #![allow(clippy::unnecessary_fold)]
+    convert_to_integers(lines)
+        .into_iter()
+        .fold(0, |x, acc| x + acc)
+}
+
 /// Accepts a vector of the lines in the exercise and returns a vector of the even numbers
 fn keep_evens(lines: Vec<String>) -> Vec<u8> {
     convert_to_integers(lines)
@@ -27,7 +37,7 @@ mod tests {
     }
 
     #[test]
-    fn exercise1_converts_to_integers() {
+    fn converts_to_integers() {
         assert_eq!(
             convert_to_integers(lines()),
             vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -35,12 +45,17 @@ mod tests {
     }
 
     #[test]
-    fn exercise1_lines_sums_correctly() {
+    fn lines_sums_correctly() {
         assert_eq!(sum_lines(lines()), 55)
     }
 
     #[test]
-    fn exercise1_retains_evens() {
+    fn sums_lines_in_mysterious_ways() {
+        assert_eq!(sum_lines(lines()), sum_lines_bonus(lines()))
+    }
+
+    #[test]
+    fn retains_evens() {
         assert_eq!(keep_evens(lines()), vec![2, 4, 6, 8, 10])
     }
 }
