bd345c30ddf9aca21ca512c9ae3b1f834cbc701a
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2023-09-07 23:13:23 +0200

    Add exercise on retaining even numbers

--- src/exercise1.rs
+++ src/exercise1.rs
@@ -1,10 +1,19 @@
-/// Accepts a vector of strings and returns a vector of integers.
+/// Accepts a vector of the lines in the exercise and returns a vector of integers
 fn convert_to_integers(lines: Vec<String>) -> Vec<u8> {
     lines.into_iter().map(|x| x.parse().unwrap()).collect()
 }
 
+/// Accepts a vector of the lines in the exercise and returns their sum
 fn sum_lines(lines: Vec<String>) -> u8 {
     convert_to_integers(lines).into_iter().sum()
+}
+
+/// Accepts a vector of the lines in the exercise and returns a vector of the even numbers
+fn keep_evens(lines: Vec<String>) -> Vec<u8> {
+    convert_to_integers(lines)
+        .into_iter()
+        .filter(|x| x % 2 == 0)
+        .collect()
 }
 
 #[cfg(test)]
@@ -28,5 +37,10 @@ mod tests {
     #[test]
     fn exercise1_lines_sums_correctly() {
         assert_eq!(sum_lines(lines()), 55)
+    }
+
+    #[test]
+    fn exercise1_retains_evens() {
+        assert_eq!(keep_evens(lines()), vec![2, 4, 6, 8, 10])
     }
 }
