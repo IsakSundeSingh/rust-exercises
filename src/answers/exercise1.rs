9858cc2fa2b122ebc616788514089cca1a4fc521
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2023-09-20 19:20:31 +0200

    Add lowercase-task

--- src/answers/exercise1.rs
+++ src/answers/exercise1.rs
@@ -1,3 +1,7 @@
 pub(crate) fn uppercase_owned_string(input: String) -> String {
     input.to_uppercase()
 }
+
+pub(crate) fn lowercase_borrowed_string(input: &str) -> String {
+    input.to_lowercase()
+}
--- src/exercise1.rs
+++ src/exercise1.rs
@@ -4,6 +4,10 @@ fn uppercase_owned_string(input: String) -> String {
     todo!()
 }
 
+fn lowercase_borrowed_string(input: &str) -> String {
+    crate::answers::exercise1::lowercase_borrowed_string(input)
+}
+
 #[cfg(test)]
 mod tests {
     // Star imports are fine when done like this, in my opinion.
@@ -19,6 +23,15 @@ mod tests {
         assert_eq!(
             uppercase_owned_string("denne håndterer jo æ ø og å".into()),
             String::from("DENNE HÅNDTERER JO Æ Ø OG Å")
+        );
+    }
+
+    #[test]
+    fn can_lowercase_and_create_new_string() {
+        assert_eq!(lowercase_borrowed_string("SHOUT"), String::from("shout"));
+        assert_eq!(
+            lowercase_borrowed_string("THIS WAS NOT AS HARD AS YOU THOUGHT"),
+            String::from("this was not as hard as you thought")
         );
     }
 }
